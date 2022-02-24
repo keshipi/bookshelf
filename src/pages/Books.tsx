685e83050cec901373072aa7f01c3d9d47763d8c
Takashi Suzuki <keshipink@gmail.com>
2022-02-24 13:07:51 +0900

    検索条件のstateを削除し、useSearchParamsを利用するように修正

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -12,44 +12,37 @@ type Result = {
   books: Book[];
 };
 
-type SearchParam = {
-  query: string;
-  page: string;
-};
-
 export const Books = () => {
   const pagePerItems = 40;
   const [result, setResult] = useState<Result>({ total: 0, books: [] });
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchParam, setSearchParam] = useState<SearchParam>({
-    query: searchParams.get('query') || '',
-    page: searchParams.get('page') || '1',
-  });
+  const [searchParams, setSearchParams] = useSearchParams(
+    createSearchParams({ query: '', page: '1' })
+  );
   const [isSearching, setIsSearching] = useState(false);
 
   const handleSearch = (query: string) => {
     if (!query) return;
     const newSearchParam = { query, page: '1' };
-    const params = createSearchParams(newSearchParam).toString();
-    setSearchParams(new URLSearchParams(params));
-    setSearchParam(newSearchParam);
+    setSearchParams(createSearchParams(newSearchParam));
   };
 
   const handlePageClick = (selectedPage: number) => {
-    const page = selectedPage.toString();
-    const newSearchParam = { ...searchParam, ...{ page } };
-    const params = createSearchParams(newSearchParam).toString();
-    setSearchParams(new URLSearchParams(params));
-    setSearchParam(newSearchParam);
+    const newSearchParam = {
+      query: searchParams.get('query') || '',
+      page: selectedPage.toString(),
+    };
+    setSearchParams(createSearchParams(newSearchParam));
   };
 
   useEffect(() => {
-    if (!searchParam.query) return;
+    if (!searchParams.get('query')) return;
     setIsSearching(true);
+    const query = searchParams.get('query');
+    const page = Number(searchParams.get('page'));
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${
-        searchParam.query
-      }&maxResults=40&startIndex=${(Number(searchParam.page) - 1) * pagePerItems}`
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=40&startIndex=${
+        (page - 1) * pagePerItems
+      }`
     )
       .then((res) => res.json())
       .then((res) =>
@@ -61,7 +54,7 @@ export const Books = () => {
       .finally(() => {
         setIsSearching(false);
       });
-  }, [searchParam]);
+  }, [searchParams]);
 
   const mapBooksFromGoogle = (res: any): Book[] => {
     return res.map((item: any, i: Number) => {
@@ -79,7 +72,7 @@ export const Books = () => {
     <>
       <Box w="100%" p={4}>
         <SearchBox
-          text={searchParam.query}
+          text={searchParams.get('query') || ''}
           onSearch={handleSearch}
           isLoading={isSearching}
         ></SearchBox>
@@ -94,7 +87,7 @@ export const Books = () => {
               totalCount={result.total}
               pageSize={pagePerItems}
               onClick={handlePageClick}
-              currentPage={Number(searchParam.page)}
+              currentPage={Number(searchParams.get('page'))}
             ></Pagination>
           </Center>
         </Box>
