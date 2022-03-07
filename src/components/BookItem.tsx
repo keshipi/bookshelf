53e1fbc43fedd4f2ee1f12dd05cb9a1affeb8054
Takashi Suzuki <keshipink@gmail.com>
2022-03-08 00:23:02 +0900

    本情報をstateとして詳細ページに渡すように修正

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -5,7 +5,7 @@ import { Box, Image, Text } from '@chakra-ui/react';
 
 export const BookItem = (props: Book) => {
   return (
-    <Link to={`/books/${props.isbn}`} className="group">
+    <Link to={`/books/${props.isbn}`} state={{book: props}} className="group">
       <Box>
         <Image src={props.image} alt={props.title} />
         <Text fontSize='md'>{props.title}</Text>
--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import {
   Box,
   Center,
-  Spinner,
   Flex,
   Image,
   Text,
@@ -15,50 +13,23 @@ import {
 
 import { Whoops404 } from '@/pages/404';
 
-export const BookDetail = () => {
-  const { id } = useParams();
-  const [book, setBook] = useState<BookDetail>();
-  const [isSearching, setIsSearching] = useState(true);
+interface BookDetailState {
+  book: Book | null;
+}
 
-  useEffect(() => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${id}`)
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.totalItems > 0) {
-          setBook({
-            id: res.items[0].id,
-            name: res.items[0].volumeInfo.title,
-            authors: res.items[0].volumeInfo.authors || [],
-            image: `https://images-na.ssl-images-amazon.com/images/P/${id}.09.LZZZZZZZ.jpg`,
-            description: res.items[0].volumeInfo.description,
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setIsSearching(false);
-      });
-  }, []);
+export const BookDetail = () => {
+  const location = useLocation();
+  console.log(location.state);
+  const state = location.state as BookDetailState;
+  const { book } = state;
 
   return (
     <>
-      {isSearching ? (
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="xl"
-          />
-        </Flex>
-      ) : book ? (
+      {book ? (
         <Flex p="8">
           <Box w="30%">
             <Center>
-              <Image src={book.image} alt={book.name} w="75%" />
+              <Image src={book.image} alt={book.title} w="75%" />
             </Center>
           </Box>
           <Box w="70%">
@@ -66,16 +37,28 @@ export const BookDetail = () => {
               <Box color="gray.500">
                 {book.authors.map((author: string) => {
                   return (
-                    <Link key={author} to={`/books?query=author:${author}`}>
-                      <Text as="span" mr={2}>{author}</Text>
+                    <Link
+                      key={author}
+                      to={`/books?query=author:` + encodeURIComponent(author)}
+                    >
+                      <Text as="span" mr={2}>
+                        {author}
+                      </Text>
                     </Link>
                   );
                 })}
               </Box>
-              <Heading as="h1">{book.name}</Heading>
-              <Text
-                dangerouslySetInnerHTML={{ __html: book.description }}
-              ></Text>
+              <Heading as="h1">{book.title}</Heading>
+              {book.descriptions.map((description: string, index: number) => {
+                return (
+                  <Text
+                    key={index}
+                    dangerouslySetInnerHTML={{
+                      __html: description.replaceAll('\n', '</br>'),
+                    }}
+                  ></Text>
+                );
+              })}
             </Stack>
           </Box>
         </Flex>
